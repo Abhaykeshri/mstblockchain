e087e2ddd0396633987382a281ba76528d7187a7
Om Palande <ompalande2151@gmail.com>
2026-04-06 18:22:23 +0530

    product

--- mstblockchain/app/page.jsx
+++ mstblockchain/app/page.jsx
@@ -1,11 +1,14 @@
 import HeroSection from './components/hero/HeroSection';
 import Navbar from './components/navbar/Navbar';
+import ProductShowcase from './components/productSection/ProductShowcase';
+// import ProductMobileCanvas from './components/productSection/ProductShowcase';
 
 export default function HomePage() {
   return (
     <main className="min-h-screen bg-white text-black relative">
       <Navbar/>
       <HeroSection />
+      <ProductShowcase/>
     </main>
   );
 }Navbar
